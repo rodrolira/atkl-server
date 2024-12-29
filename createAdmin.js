036baf437ef8778e81ddaa719e7da86c83494b12
Rodrigo Lira <rlirapizarro@gmail.com>
2024-12-28 21:15:40 -0300

    j

--- createAdmin.js
+++ createAdmin.js
@@ -1,6 +1,7 @@
 import pkg from 'bcryptjs'
 const { genSaltSync, hashSync } = pkg
 const salt = genSaltSync(10)
-const hashedPassword = hashSync('atkl', salt)
+const password = process.env.ADMIN_PASSWORD || 'default_password';
+const hashedPassword = hashSync(password, salt);
 
 console.log(hashedPassword)
